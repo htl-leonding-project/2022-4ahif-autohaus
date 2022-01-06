ff2b88d6523112610bf0b35d283d0be80a50b5fa
Christoph <christoph.handel@icloud.com>
2022-01-06 20:49:30 +0100

    added methods in the MatchResource class

--- src/main/java/at/htl/boundary/MatchResource.java
+++ src/main/java/at/htl/boundary/MatchResource.java
@@ -1,18 +1,22 @@
 package at.htl.boundary;
 
 import at.htl.entity.Match;
+import com.fasterxml.classmate.members.ResolvedParameterizedMember;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.List;
 
 @Path("/matches")
 public class MatchResource {
 
+    /**
+     * Gibt alle Matches zurück
+     * @return
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll()
@@ -21,6 +25,11 @@ public class MatchResource {
         return Response.ok(matches).build();
     }
 
+    /**
+     * Gibt ein Match per id zurück
+     * @param id
+     * @return
+     */
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -30,6 +39,11 @@ public class MatchResource {
                 .orElse(Response.status(Response.Status.NOT_FOUND).build());
     }
 
+    /**
+     * Diese Methode gibt Matches mit einem bestimmten Team zurück
+     * @param team1
+     * @return
+     */
     @GET
     @Path("team1/{team1}" )
     @Produces(MediaType.APPLICATION_JSON)
@@ -39,6 +53,39 @@ public class MatchResource {
                 .ok(Match.list("Select m from match where m.team1 = ?1 order by id",
                 "Desc",team1))
                 .build();
+    }
+
+    /**
+     * Mithilfe dieser Methode kann man ein Match hinzufügen
+     * @param match
+     * @return
+     */
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createMatch(Match match){
+        Match.persist(match);
+        if (match.isPersistent()){
+            return Response.created(URI.create("/matches"+match.id)).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    /**
+     * Diese Methode löscht Matches nach der Id.
+     * @param id
+     * @return
+     */
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteById(@PathParam("id") Long id){
+        boolean deleted = Match.deleteById(id);
+        if (deleted){
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
 }
