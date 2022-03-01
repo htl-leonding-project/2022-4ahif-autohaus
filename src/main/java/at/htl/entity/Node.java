585aca2965dee4028487e7ce36630943a613bb3a
Lukas Baumgartner <euklas7@gmail.com>
2022-03-01 15:29:50 +0100

    Merged Node and Phase with NodeGP and PhaseGP

--- src/main/java/at/htl/entity/Node.java
+++ src/main/java/at/htl/entity/Node.java
@@ -16,7 +16,8 @@ public class Node {
     Node rightNode;
     @ManyToOne
     Node parentNode;
-//    Node centerNode;  //nur bei "kleinen Finale"
+    @ManyToOne
+    Node centerNode;  //nur bei "kleinen Finale"
     @ManyToOne
     Match curMatch;
     @ManyToOne
@@ -57,6 +58,14 @@ public class Node {
         return parentNode;
     }
 
+    public Node getCenterNode() {
+        return centerNode;
+    }
+
+    public void setCenterNode(Node centerNode) {
+        this.centerNode = centerNode;
+    }
+
     public void setParentNode(Node parentNode) {
         this.parentNode = parentNode;
         if(this.parentNode.getLeftNode() == null)
@@ -76,5 +85,25 @@ public class Node {
     public void setChildMatchWinners() {
         setCurMatch(new Match(this.leftNode.getCurMatch().getWinningTeam(),
                 this.rightNode.getCurMatch().getWinningTeam()));
+    }
+
+    public String getMatchTableGP(){
+        String returnString = String.format("""
+                map Match.%svs%s {
+                %s => %d
+                %s => %d
+                }
+                """,this.getCurMatch().getTeam1().getAbbr(), this.getCurMatch().getTeam2().getAbbr(),
+                this.getCurMatch().getTeam1().getName(), this.getCurMatch().getPointsTeam1(),
+                this.getCurMatch().getTeam2().getName(), this.getCurMatch().getPointsTeam2());
+
+        return returnString;
+    }
+
+    public String getMatchNameGP(){
+        String matchString = String.format("Match.%svs%s"
+                , this.getCurMatch().getTeam1().getAbbr()
+                , this.getCurMatch().getTeam2().getAbbr());
+        return matchString;
     }
 }
