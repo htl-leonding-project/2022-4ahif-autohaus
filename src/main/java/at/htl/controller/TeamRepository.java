771df18f889303b05bb787ab74ef16fab609f109
Lukas Baumgartner <euklas7@gmail.com>
2022-06-18 11:45:21 +0200

    moved business logic to repos

--- src/main/java/at/htl/controller/TeamRepository.java
+++ src/main/java/at/htl/controller/TeamRepository.java
@@ -6,8 +6,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class TeamRepository implements PanacheRepository<Team> {
-
+    public List<Team> setTeamsForTournament(int amount){
+        List<Team> teams = new ArrayList<>();
+        for(int i=1; i<amount+1; i++){
+            teams.add(this.findById((long)i));
+        }
+        return teams;
+    }
 }
