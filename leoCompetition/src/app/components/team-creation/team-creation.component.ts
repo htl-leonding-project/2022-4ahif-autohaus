ee23d5eb171de1be3fa20116af524d15a5c7ca26
BaumgartnerLukas0922 <euklas7@gmail.com>
2022-10-11 22:19:39 +0200

    tournament-creation-verssion1

--- leoCompetition/src/app/components/team-creation/team-creation.component.ts
+++ leoCompetition/src/app/components/team-creation/team-creation.component.ts
@@ -11,25 +11,54 @@ import { Router } from '@angular/router';
 })
 export class TeamCreationComponent implements OnInit {
 
+  tournamentName: string = "";
   newTeam: Team;
+  addedTeams: Team[] = []
+  allTeams: Team[] = []
 
   constructor(private router: Router, private teamService: TeamService) { 
     this.newTeam = {id: 0, abbr: "", name: "", winAmount: 0}
   }
 
   ngOnInit(): void {
+    this.refreshTeams();
   }
 
   onSubmit(teamForm: NgForm){
     if(teamForm.valid)
       this.teamService.saveTeam(this.newTeam).subscribe({next:
         data => {
-          this.router.navigate(['/teams']);
+          this.refreshTeams();
+          this.newTeam.abbr = ""
+          this.newTeam.name = ""
         },
         error: error => {
           alert('Speichern fehlgeschlagen');
         }
       });
   
+  }
+
+  refreshTeams(){
+    this.teamService.getTeams().subscribe({next:
+      data =>{
+        this.allTeams = data;
+        this.allTeams = this.allTeams.filter((team) => this.addedTeams.find((obj) => {return obj.abbr === team.abbr})== null)
+      },
+      error: error =>{
+        alert('Error loading teams');
+      }
+    });
+
+  }
+
+  select(selected: Team){
+    this.addedTeams.push(selected);
+    this.refreshTeams();
+  }
+
+  deselect(selected: Team){
+    this.addedTeams = this.addedTeams.filter((team) => team.id !== selected.id)
+    this.refreshTeams();
   }
 }
