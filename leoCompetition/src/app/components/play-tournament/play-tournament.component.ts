bcd682cd6f43e18f3638457c9258323860d5f6f3
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-10-12 14:18:49 +0200

    pelzeder-added-matches-from-database

--- leoCompetition/src/app/components/play-tournament/play-tournament.component.ts
+++ leoCompetition/src/app/components/play-tournament/play-tournament.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Match } from 'src/app/models/match.model';
+import { MatchService } from 'src/app/services/match.service';
 
 @Component({
   selector: 'app-play-tournament',
@@ -11,9 +12,21 @@ export class PlayTournamentComponent implements OnInit {
   finishedAllMatches:Boolean=false;
   matches:Match[]=[];
 
-  constructor() { }
+  constructor(private matchService: MatchService) { }
 
   ngOnInit(): void {
+    this.refreshMatches();
   }
 
+  refreshMatches(){
+    this.matchService.getMatches().subscribe({next:
+      data =>{
+        this.matches = data;
+      },
+      error: error =>{
+        alert('Error loading matches');
+      }
+    });
+  }
+  
 }
