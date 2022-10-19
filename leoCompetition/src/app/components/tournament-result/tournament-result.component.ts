3f084b048ec6f1e0928eae62f0ebab3b5819c646
BaumgartnerLukas0922 <euklas7@gmail.com>
2022-10-19 14:50:29 +0200

    Added-Download

--- leoCompetition/src/app/components/tournament-result/tournament-result.component.ts
+++ leoCompetition/src/app/components/tournament-result/tournament-result.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -10,7 +11,10 @@ export class TournamentResultComponent implements OnInit {
 
   tournamentName:string="";
 
-  constructor(private route: ActivatedRoute, private router: Router){}
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private http: HttpClient){}
 
   ngOnInit(): void {
     this.route.params.subscribe(
