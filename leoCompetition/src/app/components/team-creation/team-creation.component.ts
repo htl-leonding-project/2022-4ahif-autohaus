1dc3acae4bafdb5eaa536beed46f192bfe878099
BaumgartnerLukas0922 <euklas7@gmail.com>
2022-12-16 09:47:58 +0100

    baumgartner-added-file-upload-restrictions

--- leoCompetition/src/app/components/team-creation/team-creation.component.ts
+++ leoCompetition/src/app/components/team-creation/team-creation.component.ts
@@ -40,6 +40,10 @@ export class TeamCreationComponent implements OnInit {
       const file = event.target.files![0];
       const reader = new FileReader();
 
+      console.log(file.type)
+
+      if(file.type == 'text/csv'){
+
       reader.readAsText(file);
       reader.onload = (e) => {
         let csv: any = reader.result;
@@ -58,33 +62,40 @@ export class TeamCreationComponent implements OnInit {
         this.lines.push(tarr);
 
         if(this.lines[0][0] == "name" && this.lines[0][1]=="abbr"){
-        let tarrR = [];
+          let tarrR = [];
+            
+            let arrl = allTextLines.length;
+            let rows = [];
+            for(let i = 1; i < arrl; i++){
+            rows.push(allTextLines[i].split(';'));
           
-          let arrl = allTextLines.length;
-          let rows = [];
-          for(let i = 1; i < arrl; i++){
-          rows.push(allTextLines[i].split(';'));
-         
-          }
-          
-          for (let j = 0; j < arrl; j++) {
-      
-              tarrR.push(rows[j]);
-              
-          }
-          this.linesR.push(tarrR);
+            }
+            
+            for (let j = 0; j < arrl; j++) {
+        
+                tarrR.push(rows[j]);
+                
+            }
+            this.linesR.push(tarrR);
 
-          console.log(this.linesR);
+            console.log(this.linesR);
 
-          for (let i = 1; i < this.linesR[0].length; i+=2) {
-            console.log(this.linesR[0][i][0]);
-            console.log(this.linesR[0][i][1])
-            this.select({id: this.newTeam.id, name: this.linesR[0][i][0], abbr: this.linesR[0][i][1].toUpperCase()});
-            this.newTeam.abbr = "";
-            this.newTeam.name = "";
+            for (let i = 1; i < this.linesR[0].length; i+=2) {
+              console.log(this.linesR[0][i][0]);
+              console.log(this.linesR[0][i][1])
+              this.select({id: this.newTeam.id, name: this.linesR[0][i][0], abbr: this.linesR[0][i][1].toUpperCase()});
+              this.newTeam.abbr = "";
+              this.newTeam.name = "";
+            }
+
           }
-
         }
+
+        this.notifier.notify('success', 'File wurde erfolgreich verarbeitet');
+
+      }
+      else{
+        this.notifier.notify('info', 'Bitte nur .csv Files hochladen');
       }
     };
     this.lines  = []; //for headings
