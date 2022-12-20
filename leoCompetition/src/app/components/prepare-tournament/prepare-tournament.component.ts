7f2fa873451c8a097b89538cdf508e024c9d1f43
BaumgartnerLukas0922 <euklas7@gmail.com>
2022-12-20 14:01:32 +0100

    baumgartner-connected-backend-team-pairing-to-frontend

--- leoCompetition/src/app/components/prepare-tournament/prepare-tournament.component.ts
+++ leoCompetition/src/app/components/prepare-tournament/prepare-tournament.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
+import { Team } from 'src/app/models/team.model';
+import { MatchService } from 'src/app/services/match.service';
+import { TournamentService } from 'src/app/services/tournament.service';
 
 @Component({
   selector: 'app-prepare-tournament',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrepareTournamentComponent implements OnInit {
 
-  constructor() { }
+  teams: Team[] = [];
+  tournamentName: string = "";
+  teamsH1: Team[] = [];
+  teamsH2: Team[] = [];
+
+  constructor(private route: ActivatedRoute,
+    private router:Router, 
+    private tournamentService: TournamentService,
+    private notifier: NotifierService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      params => {
+        this.tournamentName = params['name'];
+        this.loadData()
+      }
+    )
+
+    
+  }
+
+  loadData(){
+    this.tournamentService.getTeams(this.tournamentName).subscribe({next:
+      data =>{
+        this.teams = data;
+        this.teamsH1 = this.teams.splice(0, this.teams.length/2)
+        this.teamsH2 = this.teams.splice(this.teams.length/2-1, this.teams.length)
+      },
+      error: error =>{
+        this.notifier.notify( 'error','Teams konnten nicht geladen werden!');
+      }
+    })
+  }
+
+  startMatches(){
+
   }
 
 }
