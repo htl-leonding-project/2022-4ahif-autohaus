cfdd99d0a397f0370085da7a38d0957922c94738
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-12-20 17:11:13 +0100

    pelzeder-started-with-cdk

--- leoCompetition/src/app/components/prepare-tournament/prepare-tournament.component.ts
+++ leoCompetition/src/app/components/prepare-tournament/prepare-tournament.component.ts
@@ -4,6 +4,7 @@ import { NotifierService } from 'angular-notifier';
 import { Team } from 'src/app/models/team.model';
 import { MatchService } from 'src/app/services/match.service';
 import { TournamentService } from 'src/app/services/tournament.service';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-prepare-tournament',
@@ -12,12 +13,14 @@ import { TournamentService } from 'src/app/services/tournament.service';
 })
 export class PrepareTournamentComponent implements OnInit {
 
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
   teams: Team[] = [];
   tournamentName: string = "";
   teamsH1: Team[] = [];
   teamsH2: Team[] = [];
-  private draggedIndex: number = -1;
-  public draggedOverIndex: number = -1;
 
   constructor(private route: ActivatedRoute,
     private router:Router, 
@@ -46,33 +49,17 @@ export class PrepareTournamentComponent implements OnInit {
     })
   }
   
-  onDragStart(index: any): void {
-    this.draggedIndex = index;
-  }
-
-  allowDrop($event: any, index: any): void {
-    this.draggedOverIndex = index;
-    $event.preventDefault();
-  }
-
-  onDrop($event: any, index: any): void {
-    $event.preventDefault();
-    console.log(this.draggedOverIndex)
-    console.log(this.draggedIndex)
-    console.log($event.target)
-    const team = this.teams[this.draggedIndex];
-    this.teams.splice(this.draggedIndex, 1);
-    this.teams.splice(index, 0, team);
-    this.teamsH1 = this.teams.splice(0, this.teams.length/2);
-    this.teamsH2 = this.teams;
-    this.teamsH1.forEach(element => {
-      console.log(element.name)
-    });
-    this.teamsH2.forEach(element => {
-      console.log(element.name)
-    });
-    this.draggedIndex = -1;
-    this.draggedOverIndex = -1;
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
   startMatches(){
