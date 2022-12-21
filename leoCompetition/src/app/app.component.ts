5878e670b5ccbf53c75ced6adcd1988187058952
BaumgartnerLukas0922 <euklas7@gmail.com>
2022-12-21 10:13:10 +0100

    baumgartner-added-screen-rotation-notofication

--- leoCompetition/src/app/app.component.ts
+++ leoCompetition/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { faMobileScreen, faRotate } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'leoCompetition';
+  correctOrientation: boolean = true;
+
+  faMobileScreen = faMobileScreen;
+  faRotate = faRotate;
 
   constructor(public router: Router){
-    
+  }
+
+  ngOnInit(): void {
+    if(screen.orientation.type == "portrait-secondary" || screen.orientation.type == "portrait-primary"){
+      this.correctOrientation = false;
+    } else {
+      this.correctOrientation = true;
+    }
+
+    window.addEventListener("orientationchange", function(){
+      this.window.location.reload()
+    });
   }
 }
