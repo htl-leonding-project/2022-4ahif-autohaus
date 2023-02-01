abcbda51d34e37c9ba91445cdfbe969070dfa4e5
Christoph <christoph.handel@icloud.com>
2023-02-01 13:30:49 +0100

    debug commit

--- leoCompetition/src/environments/environment.prod.ts
+++ leoCompetition/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   URL_BASE_URL: 'https://student.cloud.htl-leonding.ac.at/c.handel/api',
-  IMAGE_URL: 'c.handel/api/images'
+  IMAGE_URL: '/c.handel/api/images'
 };
